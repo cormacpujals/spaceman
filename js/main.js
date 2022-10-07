5ebe9c6ff58ddabd635b7a9629d405601fb3b0d0
cormacpujals <cmpujals@gmail.com>
2022-10-07 11:56:53 -0700

    add initial source

---
+++ js/main.js
@@ -0,0 +1,53 @@
+//  Pseudo(pseudo)code
+
+// randomly generate from state one of Jupiter's moons
+// display the # of characters of the word
+// based on length of word: minutes of oxygen remaining
+// if correct # guesses < the length of the word, then the user must guess ->
+// while minutes remaining > 0
+// minutes remaining i-- upon incorrect guess and at 0, gameover 
+// show gameover sequence (message, audio(?))
+// if correct # guesses === length of the word, mission success! 
+
+
+/*----- constants -----*/
+
+
+/*----- state variables -----*/
+// alphabet (alphanumeric?? some moons have #s... may omit)
+// moons array
+// guesses/mission success/failure
+
+
+/*----- cached elements  -----*/
+// play/reset button
+// message: oxygen remaining
+
+
+/*----- event listeners -----*/
+// alphabet click
+// audio
+// play/reset
+
+/*----- functions -----*/
+// init()
+
+// render()
+
+// renderAlphabetBoard() 
+
+// renderMessage()
+
+// renderControls()
+
+// alphabetSelect()
+  // guards
+    // clicks outside of board
+    // clicks letter already chosen
+  // updates state
+    // num guesses left
+  // missionOutcome()
+  // render()
+
+//missionOutcome()
+  
